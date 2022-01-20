4fc15acc398bb09910c1eff7e2722ed79067d28f
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-01-20 09:37:23 +0900

    first commit

---
+++ pages/index-page.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ text }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    const text = ref('text')
+    return { text }
+  },
+})
+</script>
